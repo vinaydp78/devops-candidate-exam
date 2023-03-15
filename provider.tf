6e1c7ebcbd19ab016223efeb51758c34a97f7509
vinaydp78 <51081928+vinaydp78@users.noreply.github.com>
2023-03-15 15:36:30 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region  = "eu-west-1" # Don't change the region
-}
-
-# Add your S3 backend configuration here
-
-S3 Bucket: "3.devops.candidate.exam"
-Region: "eu-west-1"
-Key: "vinay.patange"
