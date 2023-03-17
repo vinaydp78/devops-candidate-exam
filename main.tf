0edf8266ff80d22fe4cabf064242a6972f92fc5d
VinayPatangedentsu <Vinay.Patange1@dentsu.com>
2023-03-17 15:46:21 +0530

    updated files

--- main.tf
+++ main.tf
@@ -1,24 +1,30 @@
 provider "aws" {
-  region = "eu-west-1"  # replace with the region you want to use
+  region = "us-west-1"  # replace with the region you want to use
+}
+
+
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
 
 # Create private subnet
 resource "aws_subnet" "private_subnet" {
-  vpc_id            = "vpc-0de2bfe0f5fc540e0"
+  vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
   #availability_zone = "eu-west-1" # replace with the AZ you want to use
 }
 
 # Create routing table
 resource "aws_route_table" "private_route_table" {
-  vpc_id = "vpc-0de2bfe0f5fc540e0"
+  vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     # Replace with the ID of the NAT Gateway that you want to use
     # to allow traffic to reach the internet
-    nat_gateway_id = "nat-07863fc48f5b99110"
+    nat_gateway_id = aws_nat_gateway.my_nat_gateway.id
   }
 
   tags = {
@@ -27,18 +33,18 @@ resource "aws_route_table" "private_route_table" {
 }
 
 # Create Lambda function
-resource "aws_lambda_function" "my_lambda_function_1" {
+resource "aws_lambda_function" "my_lambda_function" {
   function_name = "my-lambda-function"
   handler = "index.handler"
-  runtime = "nodejs14.x"
-  role = "arn:aws:iam::007974164823:role/DevOps-Candidate-Lambda-Role"
+  runtime = "python3.8"
+  role = aws_iam_role.lambda_execution_role.arn
   # replace with your lambda function code
   filename = "api_vinay.zip"
 
-  vpc_config {
-    security_group_ids = [aws_security_group.my_security_group.id]
-    subnet_ids         = [aws_subnet.private_subnet.id]
-  }
+  #vpc_config {
+   # security_group_ids = [aws_security_group.my_security_group.id]
+    #subnet_ids         = [aws_subnet.private_subnet.id]
+  #}
 }
 
 # Create security group
@@ -59,8 +65,43 @@ resource "aws_security_group" "my_security_group" {
 }
 
 # Attach security group to Lambda function
-#resource "aws_lambda_function" "my_lambda_function_1" {
-#  security_group_ids = [aws_security_group.my_security_group.id]
+#resource "aws_lambda_function" "my_lambda_function" {
+  #security_group_ids = aws_security_group.my_security_group.id
 #}
 
+# Create IAM role for Lambda function
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "my-lambda-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 
+  tags = {
+    Name = "my-lambda-execution-role"
+  }
+}
+
+# Create NAT Gateway
+resource "aws_nat_gateway" "my_nat_gateway" {
+  allocation_id = "eipalloc-0c03937d9a2c8890b"
+  subnet_id     = aws_subnet.private_subnet.id
+}
+
+
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "vinay"
+  }
+}
