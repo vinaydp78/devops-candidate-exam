76486a330d648ebb6ccdce38bf3e61fc57b5d651
vinaydp78 <51081928+vinaydp78@users.noreply.github.com>
2023-03-17 15:51:04 +0530

    Update api_vinay.py

--- api_vinay.py
+++ api_vinay.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     url = "https://2xfhzfbt31.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
     headers = {"Content-Type":"application/json", "X-Siemens-Auth": "test"}
     payload = {
-    "subnet_id": "subnet-01234567890abcdef",
+    "subnet_id": "subnet-0367f622a4bb48f22",
     "name": "vinay patange",
     "email": "vinaydp78@gmail.com"
     } # replace with your request parameters
